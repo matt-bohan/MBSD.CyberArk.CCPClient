33d15e1771fbd9ed1ec53e2a6f8024f7a46fe29b
Matt Bohan <matthew.bohan@gmail.com>
2025-07-17 19:13:43 +0100

    Minor tweaks

--- MBSD.CyberArk.CCPClient/CCPClient.cs
+++ MBSD.CyberArk.CCPClient/CCPClient.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Matthew Bohan
+ * Copyright 2025 Matthew Bohan
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
